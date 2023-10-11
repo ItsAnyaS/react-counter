40b938cf670bd25bb5505894bef7f715a0a127a0
anastasia <anyasirman@outlook.com>
2023-10-11 12:18:02 -0400

    added squaring button

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Count is {count}</h1>
       <button onClick={()=> setCount(prev => prev + 1)}>+</button>
       <button onClick={()=> setCount(prev => prev - 1)}>-</button>
+      <button onClick={()=> setCount(prev => prev * prev)}>^2</button>
     </div>
   );
 }
