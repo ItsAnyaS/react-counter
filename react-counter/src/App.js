f186aaa18e4d566e74b6ac9fbeb1db5be0afb63a
anastasia <anyasirman@outlook.com>
2023-10-11 12:17:11 -0400

    added minus counter

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Count is {count}</h1>
       <button onClick={()=> setCount(prev => prev + 1)}>+</button>
+      <button onClick={()=> setCount(prev => prev - 1)}>-</button>
     </div>
   );
 }
