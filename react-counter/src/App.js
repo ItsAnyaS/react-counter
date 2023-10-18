696e90985c50972c5ded60ae280cb67fa8474e2f
anastasia <anyasirman@outlook.com>
2023-10-18 16:30:07 -0400

    added button to set counter to specific number and added login to avoid null errors

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -2,6 +2,21 @@
 import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
+  const [inputNum, setInputNum] = useState()
+
+  const handleChange = (e) => {
+    let input = parseInt(e.target.value)
+    if (input){
+      setInputNum(input)
+    } else {
+      return 
+    }
+    
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setCount(inputNum)
+  }
   return (
     <div className="App">
       <h1>Count is {count}</h1>
@@ -9,6 +24,10 @@ function App() {
       <button onClick={()=> setCount(prev => prev - 1)}>-</button>
       <button onClick={()=> setCount(prev => prev * prev)}>^2</button>
       <button onClick={()=> setCount(0)}>Set to 0</button>
+      <form onChange={handleChange} onSubmit={handleSubmit}>
+        <input type="text" placeholder='Number to change to' />
+        <button>Change</button>
+      </form>
     </div>
   );
 }
